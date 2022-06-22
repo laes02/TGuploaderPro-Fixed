2ac61279eebaadc50c61840f5308a90b3b5a2f3d
laes02 <106196453+laes02@users.noreply.github.com>
2022-06-21 23:29:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -240,7 +240,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user','*')
 
         #Descomentar debajo solo si se ba a poner el usuario admin de telegram manual
-        #tl_admin_user = 'LAES2002'
+        tl_admin_user = 'LAES2002'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -559,7 +559,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #decomentar abajo y modificar solo si se va a poner el token del bot manual
-    #bot_token = 'BOT TOKEN'
+    bot_token = '5375754346:AAEggYT29A3QNg5HFVuBbVgOjUE169EzaBI'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
